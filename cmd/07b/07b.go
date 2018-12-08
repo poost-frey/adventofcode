db8cc768568e15273be7ed8237457f70de59a488
Benjamin Frey <benjamin.frey12@gmail.com>
2018-12-08 13:57:45 -0500

    fixed bug.  once a worker completes, all idle workers need to see if they can pick something up.

--- cmd/07b/07b.go
+++ cmd/07b/07b.go
@@ -8,20 +8,114 @@ import (
 	"strings"
 )
 
-const numWorkers = 2 // 2 for example. 5 for actual
-const baseTime = 0   // 0 for example. 60 for actual
+const numWorkers = 5 // 2 for example. 5 for actual
+const baseTime = 60  // 0 for example. 60 for actual
 const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 func main() {
 	// expected answer = 15
 	input := []string{
-		"Step C must be finished before step A can begin.",
-		"Step C must be finished before step F can begin.",
+		"Step L must be finished before step A can begin.",
+		"Step P must be finished before step F can begin.",
+		"Step V must be finished before step U can begin.",
+		"Step F must be finished before step S can begin.",
+		"Step A must be finished before step J can begin.",
+		"Step R must be finished before step K can begin.",
+		"Step Z must be finished before step T can begin.",
+		"Step G must be finished before step W can begin.",
+		"Step H must be finished before step K can begin.",
+		"Step T must be finished before step U can begin.",
+		"Step K must be finished before step B can begin.",
+		"Step C must be finished before step Y can begin.",
+		"Step W must be finished before step N can begin.",
+		"Step E must be finished before step M can begin.",
+		"Step N must be finished before step J can begin.",
+		"Step B must be finished before step S can begin.",
+		"Step O must be finished before step D can begin.",
+		"Step X must be finished before step D can begin.",
+		"Step M must be finished before step Q can begin.",
+		"Step S must be finished before step J can begin.",
+		"Step U must be finished before step Y can begin.",
+		"Step I must be finished before step J can begin.",
+		"Step D must be finished before step J can begin.",
+		"Step Q must be finished before step Y can begin.",
+		"Step J must be finished before step Y can begin.",
+		"Step Z must be finished before step D can begin.",
+		"Step K must be finished before step E can begin.",
+		"Step U must be finished before step J can begin.",
+		"Step I must be finished before step Y can begin.",
 		"Step A must be finished before step B can begin.",
-		"Step A must be finished before step D can begin.",
-		"Step B must be finished before step E can begin.",
-		"Step D must be finished before step E can begin.",
+		"Step B must be finished before step Q can begin.",
+		"Step Z must be finished before step S can begin.",
 		"Step F must be finished before step E can begin.",
+		"Step B must be finished before step I can begin.",
+		"Step C must be finished before step S can begin.",
+		"Step O must be finished before step S can begin.",
+		"Step V must be finished before step O can begin.",
+		"Step C must be finished before step B can begin.",
+		"Step G must be finished before step M can begin.",
+		"Step O must be finished before step Y can begin.",
+		"Step H must be finished before step N can begin.",
+		"Step D must be finished before step Y can begin.",
+		"Step Z must be finished before step O can begin.",
+		"Step K must be finished before step W can begin.",
+		"Step M must be finished before step Y can begin.",
+		"Step O must be finished before step J can begin.",
+		"Step P must be finished before step E can begin.",
+		"Step C must be finished before step Q can begin.",
+		"Step I must be finished before step D can begin.",
+		"Step F must be finished before step I can begin.",
+		"Step W must be finished before step B can begin.",
+		"Step W must be finished before step M can begin.",
+		"Step N must be finished before step D can begin.",
+		"Step Z must be finished before step M can begin.",
+		"Step M must be finished before step U can begin.",
+		"Step R must be finished before step I can begin.",
+		"Step S must be finished before step Y can begin.",
+		"Step L must be finished before step B can begin.",
+		"Step S must be finished before step D can begin.",
+		"Step R must be finished before step G can begin.",
+		"Step U must be finished before step D can begin.",
+		"Step C must be finished before step N can begin.",
+		"Step R must be finished before step T can begin.",
+		"Step K must be finished before step U can begin.",
+		"Step W must be finished before step E can begin.",
+		"Step H must be finished before step E can begin.",
+		"Step X must be finished before step M can begin.",
+		"Step G must be finished before step I can begin.",
+		"Step C must be finished before step U can begin.",
+		"Step N must be finished before step B can begin.",
+		"Step X must be finished before step S can begin.",
+		"Step G must be finished before step H can begin.",
+		"Step T must be finished before step X can begin.",
+		"Step P must be finished before step N can begin.",
+		"Step B must be finished before step Y can begin.",
+		"Step S must be finished before step Q can begin.",
+		"Step C must be finished before step E can begin.",
+		"Step F must be finished before step D can begin.",
+		"Step H must be finished before step J can begin.",
+		"Step B must be finished before step U can begin.",
+		"Step B must be finished before step J can begin.",
+		"Step P must be finished before step I can begin.",
+		"Step N must be finished before step X can begin.",
+		"Step M must be finished before step J can begin.",
+		"Step X must be finished before step I can begin.",
+		"Step L must be finished before step P can begin.",
+		"Step T must be finished before step B can begin.",
+		"Step T must be finished before step K can begin.",
+		"Step D must be finished before step Q can begin.",
+		"Step W must be finished before step X can begin.",
+		"Step A must be finished before step Y can begin.",
+		"Step G must be finished before step D can begin.",
+		"Step R must be finished before step Z can begin.",
+		"Step U must be finished before step Q can begin.",
+		"Step G must be finished before step O can begin.",
+		"Step G must be finished before step Q can begin.",
+		"Step G must be finished before step Y can begin.",
+		"Step P must be finished before step Y can begin.",
+		"Step I must be finished before step Q can begin.",
+		"Step F must be finished before step C can begin.",
+		"Step L must be finished before step K can begin.",
 	}
 
 	rules := parseInput(input)
@@ -56,14 +150,12 @@ func main() {
 				if done != "" {
 					active = strings.Replace(active, done, "", 1)
 					sequence += done
-					nextStep := getNextStep(rules, remaining, active)
-					if nextStep != "" {
-						remaining = strings.Replace(remaining, nextStep, "", 1)
-						w.setJob(nextStep)
-						active += nextStep
-					}
 				}
-			} else {
+			}
+		}
+		for _, w := range workers {
+			working := w.isBusy()
+			if !working {
 				nextStep := getNextStep(rules, remaining, active)
 				if nextStep != "" {
 					remaining = strings.Replace(remaining, nextStep, "", 1)
